353c171d4b024082acae78aa3f325155336e3ed8
Illusion 705 <65733710+Illusion705@users.noreply.github.com>
2022-03-05 01:25:00 +0000

    Added register form

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,39 @@
 // display header
 $("header").load("/public/components/header/header.html");
+
+// loading button animation
+class LoadingAnimation {
+  constructor(element, size = 20) {
+    this.element = element;
+    this.initialHeight = element.outerHeight();
+    this.initialWidth = element.outerWidth();
+    this.initialPadding = (element.innerWidth() - element.width()) / 2;
+    this.initialDisplay = element.css("display");
+    this.text = element.text();
+    this.size = size;
+  }
+
+  start() {
+    const loadingCircle = $("<div id=\"loading-circle\"></div>");
+    this.element.css("height", this.initialHeight + "px");
+    this.element.css("width", this.initialWidth + "px");
+    this.element.css("display", "flex");
+    this.element.css("justify-content", "center");
+    this.element.css("align-items", "center");
+    this.element.css("padding", "0");
+    this.element.empty();
+    this.element.append(loadingCircle);
+
+    if (this.size != 20) {
+      this.element.children().css("height", this.size + "px");
+      this.element.children().css("width", this.size + "px");
+    }
+  }
+
+  end() {
+    this.element.empty();
+    this.element.text(this.text);
+    this.element.css("padding", this.initialPadding);
+    this.element.css("display", this.initialDisplay);
+  }
+}
